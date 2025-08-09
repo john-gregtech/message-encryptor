4b67f1c39203564ec9928618d566b4619aba15cf
john-gregtech <williamtberry05@gmail.com>
2025-08-10 07:29:01 +1000

    this is a test to see how to commit

--- EncryptionProgram/EncryptionProgram/main.cpp
+++ EncryptionProgram/EncryptionProgram/main.cpp
@@ -2,8 +2,12 @@
 #include "EncryptorCore.hpp"
 
 
+
 int main() {
     
     std::cout << EncryptionWrapperForOpenSSL::prototypeFunction_returnNumber(78) << "\n";
+
+    //This was edited on my computer and pushed to Github.
+
     return 0;
 }
